b5dd4d9d0ef4c72ada26c6f3ff039b6b4c9a49b5
DwiRizqi0 <73012562+DwiRizqi0@users.noreply.github.com>
2022-03-03 21:45:10 +0700

    Menambahkan Event Pada Email dan Password

--- app/src/main/java/com/example/sampleconstraintlayout/MainActivity.java
+++ app/src/main/java/com/example/sampleconstraintlayout/MainActivity.java
@@ -3,12 +3,53 @@ package com.example.sampleconstraintlayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Deklarasi variable untuk button
+    Button btnLogin;
+
+    //Deklarasi variabel untuk EditText
+    EditText edemail, edpassword;
+
+    //Deklarasi variabel untuk menyimpan email dan password
+    String nama, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Menghubungkan variabel btnLogin dengan componen button pada layout
+        btnLogin=findViewById(R.id.btSignin);
+
+        //Menghubungkan variabel edemail dengan componen button pada layout
+        edemail=findViewById(R.id.edEmail);
+
+        //Menghubungkan variabel edpassword dengan componen button pada layout
+        edpassword=findViewById(R.id.edPassword);
+
+        //membuat fungsi onclick pada button btnLogin
+        btnLogin.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //menyimpan input user di edittext email kedalam variabel nama
+                nama = edemail.getText().toString();
+
+                //menyimpan input user di edittext password kedalam variabel password
+                password = edpassword.getText().toString();
+
+                //membuat variabel toast dan membuat toast dengan menambahkan variabel nama dan password
+                Toast t = Toast.makeText(getApplicationContext(),
+                        "email anda: "+nama+"dan Password anda: "+password+"", Toast.LENGTH_LONG);
+                //menampilkan toast
+                t.show();
+            }
+        }));
     }
 }
